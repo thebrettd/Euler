fe79c26bf18ac3a650592cd2740dd707d4aa6c34
thebrettd <Btd123!>
2012-11-19 17:28:41 -0800

    some refactoring

---
+++ src/Move.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: brett
+ * Date: 11/19/12
+ * Time: 3:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Move {
+    int newRank;
+    int newFile;
+
+    public Move(int newRank, int newFile) {
+        this.newRank = newRank;
+        this.newFile = newFile;
+    }
+}
