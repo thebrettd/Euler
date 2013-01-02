e44cad99f4e42c434c0b172312aa94fedae12f48
thebrettd <Btd123!>
2013-01-01 22:58:28 -0800

    problem 11

---
+++ src/Coordinate.java
@@ -0,0 +1,27 @@
+public class Coordinate{
+
+    int xpos;
+    int ypos;
+
+    public Coordinate(int x,int y){
+        xpos = x;
+        ypos = y;
+    }
+
+    public int getXpos() {
+        return xpos;
+    }
+
+    public void setXpos(int xpos) {
+        this.xpos = xpos;
+    }
+
+    public int getYpos() {
+        return ypos;
+    }
+
+    public void setYpos(int ypos) {
+        this.ypos = ypos;
+    }
+
+}
