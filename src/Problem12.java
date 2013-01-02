4ca40c4284ea240b540b3122b67483266af3bbcf
thebrettd <Btd123!>
2013-01-02 13:17:02 -0800

    problem 12

--- src/Problem12.java
+++ src/Problem12.java
@@ -1,4 +1,10 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Problem12 {
+
+    private static HashMap<BigInteger, PossiblePrime> primesList = SieveOfEratosthenes.generatePrimes(new BigInteger("2000000"));
 
     public static void main(String[] args){
 
@@ -11,11 +17,45 @@ public class Problem12 {
             triangleValue = TriangleNumber.computeTriangleNumber(currTriangleNumber);
             //System.out.println(currTriangleNumber + ": " + triangleValue);
 
-            numDivisors = calculateNumDivisorsSlow(triangleValue);
+            numDivisors = calculateNumDivisorsPrimeFactorization(triangleValue);
 
         }while(numDivisors <= 500);
 
         System.out.println("The first triangle number with > 500 divisors: " + triangleValue);
+
+    }
+
+    /**
+     * Calculate number of divisors using prime factorization
+     * http://mathcentral.uregina.ca/QQ/database/QQ.02.06/joe1.html
+     * @param triangleValue
+     * @return
+     */
+    private static int calculateNumDivisorsPrimeFactorization(int triangleValue) {
+        BigInteger num = new BigInteger(Integer.toString(triangleValue));
+
+        System.out.println("Calculating primeFactors for " + triangleValue);
+        ArrayList<BigInteger> primeFactors = new  ArrayList<BigInteger>();
+        PrimeFactors.factorize(num, primeFactors, primesList);
+
+        HashMap<BigInteger, Integer> factorsCount = new HashMap<BigInteger,Integer>();
+
+        //loop over the primeFactors, counting the number of times each prime factor occurs in the list.
+        for (BigInteger primeFactor : primeFactors) {
+            if (factorsCount.containsKey(primeFactor)){
+                Integer oldCount = factorsCount.get(primeFactor);
+                factorsCount.put(primeFactor,oldCount + 1);
+            }else{
+                factorsCount.put(primeFactor,2);
+            }
+        }
+
+        int numFactors = 1;
+        for (Integer factorCount : factorsCount.values()){
+            numFactors *= factorCount;
+        }
+
+        return numFactors;
 
     }
 
--- src/TriangleNumber.java
+++ src/TriangleNumber.java
@@ -1,5 +1,5 @@
 public class TriangleNumber {
-    static int computeTriangleNumber(int currTriangleNumber) {
+    public static int computeTriangleNumber(int currTriangleNumber) {
         int sum = 0;
         for (int i = 1; i <= currTriangleNumber; i++) {
             sum += i;
