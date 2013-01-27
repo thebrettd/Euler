a17c1b7a43994120cb2c8c66dc9499f5c3e041fe
thebrettd <Btd123!>
2013-01-26 19:52:07 -0800

    problem 13

---
+++ src/PowerSum.java
@@ -0,0 +1,12 @@
+import utils.BigIntUtils;
+
+import java.math.BigInteger;
+
+public class PowerSum {
+
+    public static BigInteger powerSum(int base, int power){
+        BigInteger two = BigInteger.ONE.add(BigInteger.ONE);
+        BigInteger value = two.pow(power);
+        return BigIntUtils.sumOfDigits(value);
+    }
+}
---
+++ src/Problem16.java
@@ -0,0 +1,6 @@
+public class Problem16 {
+
+    public static void main(String[] args){
+        System.out.println("Power sum of 2 to the 15: " + PowerSum.powerSum(2,1000));
+    }
+}
